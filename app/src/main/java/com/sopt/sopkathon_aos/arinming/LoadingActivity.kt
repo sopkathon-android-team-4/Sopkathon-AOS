228d1f6d42283c8332fabc8a0605ad020b8170ae
arinming <jyandysys1@naver.com>
2024-05-19 06:30:20 +0900

    feat: 인텐트로 걱정 넘기기

--- app/src/main/java/com/sopt/sopkathon_aos/arinming/LoadingActivity.kt
+++ app/src/main/java/com/sopt/sopkathon_aos/arinming/LoadingActivity.kt
@@ -3,6 +3,7 @@ package com.sopt.sopkathon_aos.arinming
 import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.sopkathon_aos.databinding.ActivityLoadingBinding
 import com.sopt.sopkathon_aos.hyunjinn.ConnectInsta.ConnectInstaActivity
@@ -14,7 +15,11 @@ class LoadingActivity : AppCompatActivity() {
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, ConnectInstaActivity::class.java)
+        val concerns = intent.getStringExtra("concerns")
+
+        val intent = Intent(this, ConnectInstaActivity::class.java).apply {
+            putExtra("concerns", concerns)
+        }
 
 
         binding.lottie.addAnimatorListener(object : Animator.AnimatorListener {
